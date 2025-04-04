e464a1eb2072f47b4e232a23502f34d359cace7d
CsPauloNoob <ps616131@gmail.com>
2025-04-03 21:41:43 -0500

    tabela incorporada

---
+++ CriptoPage/Infra/BinanceApiService.cs
@@ -0,0 +1,43 @@
+﻿using Binance.Net.Clients;
+using Binance.Net.Enums;
+using CriptoPage.Model;
+using CryptoExchange.Net.CommonObjects;
+
+namespace CriptoPage.Infra
+{
+    public static class BinanceApiService
+    {
+        public static async Task<Btc_Brl_Diario> BtcCotacaoDiaria()
+        {
+            using var client = new BinanceRestClient();
+
+            var symbol = "BTCBRL";
+
+            // Preço atual
+            var currentPriceResult = await client.SpotApi.ExchangeData.GetPriceAsync(symbol);
+
+            // Preço de 24h atrás (via candle de 1 dia atrás)
+            //var yesterday = DateTime.UtcNow.Date.AddDays(-1);
+            //var candleResult = await client.SpotApi.ExchangeData.GetKlinesAsync(
+            //    symbol,
+            //    KlineInterval.OneDay,
+            //    yesterday,
+            //    yesterday.AddDays(1),
+            //    1
+            //);
+            client.Dispose();
+            return new Btc_Brl_Diario { Date = DateTime.UtcNow, Close = (double)currentPriceResult.Data.Price, Name = "Bitcoin", Symbol = symbol };
+
+        }
+
+        public static async Task<double> CotacaoRealUsdt()
+        {
+            using var client = new BinanceRestClient();
+            var symbol = "USDTBRL";
+            // Preço atual
+            var currentPriceResult = await client.SpotApi.ExchangeData.GetPriceAsync(symbol);
+
+            return (double)currentPriceResult.Data.Price;
+        }
+    }
+}
---
+++ CriptoPage/Model/Btc_Brl_Diario.cs
@@ -0,0 +1,10 @@
+﻿namespace CriptoPage.Model
+{
+    public class Btc_Brl_Diario
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public DateTime Date { get; set; }
+        public double Close { get; set; }
+    }
+}
