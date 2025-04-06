71ba85aec624820ddfbd705db54a003bf4f695b6
CsPauloNoob <ps616131@gmail.com>
2025-04-05 23:31:21 -0500

    implementado algumas coisas

--- CriptoPage/Model/CoinData.cs
+++ CriptoPage/Model/CoinData.cs
@@ -5,7 +5,10 @@
         public string Name { get; set; }
         public string Symbol { get; set; }
         public DateTime Date { get; set; }
+        //Ultimo preço do dia
         public double Last_Price_Data { get; set; }
+        //Primeiro preço do dia
+        public double? FirstDayPrice { get; set; }
         public double PercentVariation { get; set; }
     }
 }
